6893ebbbe346c44481def17ec2136bae54681729
Dean Lovett <dean.lovett@gmail.com>
2019-05-14 13:11:47 -0700

    Added bootstrap style and cleaned up logic for big bills

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 // Write your JavaScript here
 
-const change = [
+const big_bills = [
     ['fifties-output', 5000],
     ['twenties-output',2000],
     ['tens-output',    1000],
-    ['fives-output',    500],
+    ['fives-output',    500]
+];
+const small_change = [
     ['dollars-output',  100],
     ['quarters-output',  25],
     ['dimes-output',     10],
@@ -13,19 +15,17 @@ const change = [
 ];
 const name = 0;  // first element is the ID for the output field
 const value = 1; // second element is the value of that bill or coin, in pennies
-const largeBillThreshold = 100;
-var  enableLargeBills = false;
+
+let change = small_change;  // at start, let us only use dollar bills and not the larger bills
+var  enableLargeBills = false; // at start, only use singles. No large bills.
+
+document.getElementById('amount-due').focus();
 
 function calculateChange(paid,due) {
     var change_left = paid*100 - due*100; // change left to be accounted for... in pennies
-
     change.forEach((a) => {
-        if( (!enableLargeBills && (a[value] > largeBillThreshold )) ) {
-            document.getElementById(a[name]).innerText = 0;
-        } else {
-            document.getElementById(a[name]).innerText = Math.floor(change_left / a[value]);
-            change_left = Math.ceil(change_left % a[value]);
-        }
+        document.getElementById(a[name]).innerText = Math.floor(change_left / a[value]);
+        change_left = Math.ceil(change_left % a[value]);
     });
 }
 
@@ -37,15 +37,20 @@ function handleClickEvent(e) {
   
 document.getElementById('calculate-change').onclick = handleClickEvent;
 
-function handleClickEventLargeBillToggle(e){
-    if( enableLargeBills ){
+// below is bonus project code to handle optionally using large bills for change.
+
+document.getElementById('enable-large-bills').onclick = ((e)=>{
+    if( enableLargeBills ){ // disable the large bills
         enableLargeBills = false;
+        // set big bills to zero
+        big_bills.forEach((a) => {document.getElementById(a[name]).innerText = 0;});
+        change = small_change;
         document.getElementById('enable-large-bills').innerText = 'Click to enable large bills';
-    } else {
+    } else { // enable the large bills
         enableLargeBills = true;
+        change = big_bills.concat(small_change);
         document.getElementById('enable-large-bills').innerText = 'Click to disable large bills';
     }
+    // oh hey, we just changed state let's just go ahead and re-calc the bills and change due
     handleClickEvent(e);
-}
-
-document.getElementById('enable-large-bills').onclick = handleClickEventLargeBillToggle;
+});
